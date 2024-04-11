babec8de8996c658eacfaca9e2afe614b16932f3
ismayilovcode <ismayilovterlan508@gmail.com>
2024-04-11 16:54:43 +0400

    added task

---
+++ index.js
@@ -0,0 +1,30 @@
+function changeColor() {
+  let bcg = document.body.style.backgroundColor;
+
+  document.body.style.backgroundColor = localStorage.getItem("changeColor");
+  if (bcg == "") {
+    document.body.style.backgroundColor = "black";
+  } else if (bcg == "black") {
+    document.body.style.backgroundColor = "red";
+  } else if (bcg == "red") {
+    document.body.style.backgroundColor = "yellow";
+  } else if (bcg == "yellow") {
+    document.body.style.backgroundColor = "";
+  }
+
+  let clr = document.getElementById("change").style.color;
+  let bcg2 = document.getElementById("change").style.backgroundColor;
+  if (clr == "" && bcg2 == "") {
+    document.getElementById("change").style.color = "red";
+    document.getElementById("change").style.backgroundColor = "white";
+  } else if (clr == "red" && bcg2 == "white") {
+    document.getElementById("change").style.color = "white";
+    document.getElementById("change").style.backgroundColor = "orange";
+  } else if (clr == "white" && bcg2 == "orange") {
+    document.getElementById("change").style.color = "orange";
+    document.getElementById("change").style.backgroundColor = "black";
+  } else if (clr == "orange" && bcg2 == "black") {
+    document.getElementById("change").style.color = "";
+    document.getElementById("change").style.backgroundColor = "";
+  }
+}
